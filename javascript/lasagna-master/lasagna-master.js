409373ee150202d3ff956d84833d506faebeda1f
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-06 15:46:45 -0500

    js: lasagna master

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -1,4 +1,7 @@
 /// <reference path="./global.d.ts" />
+
+import { allowedNodeEnvironmentFlags } from "process";
+
 // @ts-check
 
 /**
@@ -10,3 +13,49 @@
  *   ...
  * }
  */
+
+export function cookingStatus(timeOnTimer) {
+  if (timeOnTimer === 0) {
+    return "Lasagna is done.";
+  } else if (timeOnTimer > 0) {
+    return "Not done, please wait.";
+  } else {
+    return "You forgot to set the timer.";
+  }
+}
+
+export function preparationTime(layers, avgPrepTime = 2) {
+  return layers.length * avgPrepTime;
+}
+
+export function quantities(layers) {
+  let noodles = 0;
+  let sauce = 0;
+
+  for (const layer of layers) {
+    if (layer === "noodles") {
+      noodles += 50;
+    } else if (layer === "sauce") {
+      sauce += 0.2;
+    }
+  }
+
+  return {
+    noodles,
+    sauce,
+  };
+}
+
+export function addSecretIngredient(friendList, ownList) {
+  ownList.push(friendList[friendList.length - 1]);
+}
+
+export function scaleRecipe(recipe, scale) {
+  let scaledRecipe = {};
+
+  for (const layer in recipe) {
+    scaledRecipe[layer] = recipe[layer] * (scale / 2);
+  }
+
+  return scaledRecipe;
+}
