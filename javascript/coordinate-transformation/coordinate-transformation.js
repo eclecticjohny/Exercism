51b0eab2679df5c955038948d755e24f06e585b0
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-08 08:17:10 -0500

    js: coordinate transformation

--- javascript/coordinate-transformation/coordinate-transformation.js
+++ javascript/coordinate-transformation/coordinate-transformation.js
@@ -15,7 +15,9 @@
  *  translated coordinate pair in the form [x, y]
  */
 export function translate2d(dx, dy) {
-  throw new Error('Implement the translate2d function');
+  return function (x, y) {
+    return [x + dx, y + dy];
+  };
 }
 
 /**
@@ -29,7 +31,9 @@ export function translate2d(dx, dy) {
  *  scaled coordinate pair in the form [x, y]
  */
 export function scale2d(sx, sy) {
-  throw new Error('Implement the scale2d function');
+  return function (x, y) {
+    return [x * sx, y * sy];
+  };
 }
 
 /**
@@ -43,7 +47,10 @@ export function scale2d(sx, sy) {
  *  transformed coordinate pair in the form [x, y]
  */
 export function composeTransform(f, g) {
-  throw new Error('Implement the composeTransform function');
+  return function (x, y) {
+    const [fOne, fTwo] = f(x, y);
+    return g(fOne, fTwo);
+  };
 }
 
 /**
@@ -56,5 +63,15 @@ export function composeTransform(f, g) {
  *  if the arguments are the same on subsequent calls, or compute a new result if they are different.
  */
 export function memoizeTransform(f) {
-  throw new Error('Implement the memoizeTransform function');
+  let lastArgs = { lax: 0, lay: 0 };
+  let lastResult;
+
+  return function (x, y) {
+    if (lastArgs.lax === x && lastArgs.lay === y) {
+      return lastResult;
+    }
+
+    lastArgs = { lax: x, lay: y };
+    return (lastResult = f(x, y));
+  };
 }
