d6aff3dcd66d0eb7781f10a1ac4c9eaf1712ee84
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-18 10:30:53 -0500

    ts: resistor color

--- typescript/resistor-color/resistor-color.ts
+++ typescript/resistor-color/resistor-color.ts
@@ -1,5 +1,16 @@
-export const colorCode = () => {
-  throw new Error('Delete this line and implement this function')
-}
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
 
-export const COLORS = undefined
+export const colorCode = (code: string): number => {
+  return COLORS.indexOf(code);
+};
