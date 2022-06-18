ff7854df1eeab8fd0b5bf6b719c52cab45439a5d
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-18 11:38:31 -0500

    rs: dnd character

--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -1,9 +1,29 @@
 export class DnDCharacter {
+  readonly hitpoints: number;
+  readonly strength: number;
+  readonly dexterity: number;
+  readonly constitution: number;
+  readonly intelligence: number;
+  readonly wisdom: number;
+  readonly charisma: number;
+
+  constructor() {
+    this.strength = DnDCharacter.generateAbilityScore();
+    this.dexterity = DnDCharacter.generateAbilityScore();
+    this.constitution = DnDCharacter.generateAbilityScore();
+    this.intelligence = DnDCharacter.generateAbilityScore();
+    this.wisdom = DnDCharacter.generateAbilityScore();
+    this.charisma = DnDCharacter.generateAbilityScore();
+    this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
+  }
+
   public static generateAbilityScore(): number {
-    throw new Error('Remove this statement and implement this function')
+    const min = 3;
+    const max = 18;
+    return Math.random() * (max - min) + min;
   }
 
   public static getModifierFor(abilityValue: number): number {
-    throw new Error('Remove this statement and implement this function')
+    return Math.floor((abilityValue - 10) / 2);
   }
 }
