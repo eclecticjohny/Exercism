64a3167a9e889f984be1898c7797bbbe223c6443
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-18 12:44:45 -0500

    ts: word count

--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -1,3 +1,21 @@
-export function count() {
-  throw new Error('Remove this statement and implement this function')
+// export function count(str: string): Map<string, number> {
+//   return str
+//     .trim()
+//     .split(/\s+/)
+//     .map((s) => s.toLowerCase())
+//     .reduce((map, word) => {
+//       map.set(word, (map.get(word) || 0) + 1);
+//       return map;
+//     }, new Map<string, number>());
+// }
+
+export function count(phrase: string): Map<string, number> {
+  const words = phrase.toLowerCase().match(/[\S]+/g) ?? [];
+  let wordCounts = new Map<string, number>();
+
+  for (let word of words) {
+    wordCounts.set(word, (wordCounts.get(word) ?? 0) + 1);
+  }
+
+  return wordCounts;
 }
