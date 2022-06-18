2a4516e7968886e9e042cccf92da5f91dfe0c728
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-18 10:42:04 -0500

    ts: rna transcription

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,3 +1,21 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
+export function toRna(strand: string): string {
+  return strand
+    .split("")
+    .map((dnaChar) => toRnaChar(dnaChar))
+    .join("");
+}
+
+function toRnaChar(char: string): string {
+  switch (char) {
+    case "C":
+      return "G";
+    case "G":
+      return "C";
+    case "A":
+      return "U";
+    case "T":
+      return "A";
+    default:
+      throw new Error("Invalid input DNA.");
+  }
 }
