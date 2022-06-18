c3b8f986433c032b09c5440bdbe8a29cb063c222
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-18 13:37:36 -0500

    ts: roman numerals

--- typescript/roman-numerals/roman-numerals.ts
+++ typescript/roman-numerals/roman-numerals.ts
@@ -1,3 +1,37 @@
-export const toRoman = () => {
-  throw new Error('Remove this statement and implement this function')
-}
+const romanMapping: { num: number; roman: string }[] = [
+  { num: 1000, roman: "M" },
+  { num: 900, roman: "CM" },
+  { num: 500, roman: "D" },
+  { num: 400, roman: "CD" },
+  { num: 100, roman: "C" },
+  { num: 90, roman: "XC" },
+  { num: 50, roman: "L" },
+  { num: 40, roman: "XL" },
+  { num: 10, roman: "X" },
+  { num: 9, roman: "IX" },
+  { num: 5, roman: "V" },
+  { num: 4, roman: "IV" },
+  { num: 1, roman: "I" },
+];
+
+// export const toRoman = (inputNumber: number): string => {
+//   return romanMapping.reduce<string>((res, { num, roman }) => {
+//     while (inputNumber >= num) {
+//       res += roman;
+//       inputNumber -= num;
+//     }
+//     return res;
+//   }, "");
+// };
+
+export const toRoman = (inputNumber: number): string => {
+  let res = "";
+  for (const { num, roman } of romanMapping) {
+    while (inputNumber >= num) {
+      res += roman;
+      inputNumber -= num;
+    }
+  }
+
+  return res;
+};
