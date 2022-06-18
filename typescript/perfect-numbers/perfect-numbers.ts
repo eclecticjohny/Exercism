ad5fdd1cdf3398b5ccbe0441bf5c5b6ce959c5d8
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-18 16:50:18 -0500

    ts: perfect numbers

--- typescript/perfect-numbers/perfect-numbers.ts
+++ typescript/perfect-numbers/perfect-numbers.ts
@@ -1,3 +1,57 @@
-export function classify() {
-  throw new Error('Remove this statement and implement this function')
+// function getFactors(number: number): number[] {
+//   return Array.from(Array(number + 1), (_, i) => i).filter(
+//     (i) => number % i === 0
+//   );
+// }
+
+// function* numberFactorGenerator(number: number): Generator<number> {
+//   let i = 0;
+//   while (i <= number) {
+//     if (number % i === 0) {
+//       yield i;
+//     }
+//     i++;
+//   }
+// }
+
+function factorialize(num: number): number[] {
+  let fac: number[] = [],
+    i = 1,
+    ind = 0;
+
+  while (i <= Math.floor(Math.sqrt(num))) {
+    //inserting new elements in the middle using splice
+    if (num % i === 0) {
+      fac.splice(ind, 0, i);
+      if (i !== num / i) {
+        fac.splice(-ind, 0, num / i);
+      }
+      ind++;
+    }
+    i++;
+  }
+
+  //swapping first and last elements
+  let temp = fac[fac.length - 1];
+  fac[fac.length - 1] = fac[0];
+  fac[0] = temp;
+
+  // nice sorted array of factors
+  return fac;
+}
+
+export function classify(givenNumber: number): string {
+  if (givenNumber <= 0) {
+    throw new Error("Classification is only possible for natural numbers.");
+  }
+
+  const aliquotSum = factorialize(givenNumber)
+    .slice(0, -1)
+    .reduce((acc, curr) => acc + curr, 0);
+
+  return aliquotSum === givenNumber
+    ? "perfect"
+    : aliquotSum > givenNumber
+    ? "abundant"
+    : "deficient";
 }
