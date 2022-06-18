95fbc089de193df1a557eb8c8e24a0b0ecbee7ed
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-18 12:56:17 -0500

    ts: armstrong numbers

--- typescript/armstrong-numbers/armstrong-numbers.ts
+++ typescript/armstrong-numbers/armstrong-numbers.ts
@@ -1,3 +1,19 @@
-export function isArmstrongNumber(number: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+// export function isArmstrongNumber(input: number): boolean {
+//   const digits = input.toString().split("");
+
+//   let sum = 0;
+//   for (const digit of digits) {
+//     sum += Math.pow(Number(digit), digits.length);
+//   }
+
+//   return sum === input;
+// }
+
+export function isArmstrongNumber(input: number): boolean {
+  const digits = input.toString().split("");
+  const sum = digits.reduce(
+    (acc, curr) => acc + Math.pow(Number(curr), digits.length),
+    0
+  );
+  return sum === input;
 }
