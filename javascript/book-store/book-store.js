295fa718b482ed5f0c57bfe30db41aeb98f9df22
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-17 20:01:43 -0500

    js: book store

--- javascript/book-store/book-store.js
+++ javascript/book-store/book-store.js
@@ -3,6 +3,41 @@
 // convenience to get you started writing code faster.
 //
 
+const BOOK_PRICE = 800;
+const PERCENTS = [0, 0, 5, 10, 20, 25];
 export const cost = (books) => {
-  throw new Error('Remove this statement and implement this function');
+  return discount(bookCount(books));
 };
+function bookCount(books) {
+  return books
+    .reduce((counts, book) => {
+      counts[book] = (counts[book] ?? 0) + 1;
+      return counts;
+    }, [])
+    .filter((c) => c)
+    .sort((a, b) => b - a);
+}
+function discount(counts) {
+  switch (counts.length) {
+    case 0:
+      return 0;
+    case 1:
+      return counts[0] * BOOK_PRICE;
+    default:
+      return Math.min(
+        ...Array(counts.length)
+          .fill(0)
+          .map(
+            (_, i) =>
+              ((i + 1) * BOOK_PRICE * (100 - PERCENTS[i + 1])) / 100 +
+              discount(remainingBookCount(counts, i + 1))
+          )
+      );
+  }
+}
+function remainingBookCount(counts, n) {
+  return counts
+    .map((count, i) => (i < n ? count - 1 : count))
+    .filter((n) => n)
+    .sort((a, b) => b - a);
+}
