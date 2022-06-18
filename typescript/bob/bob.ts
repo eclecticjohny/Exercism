f6377fdced68617af8245db848050ccd1941b727
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-18 12:03:15 -0500

    ts: reverse string

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,3 +1,34 @@
-export function hey(message: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+const LOWER_ALPHA = "abcdefghijklmnopqrstuvwxyz";
+
+export function hey(message: string): string {
+  const trimmedMessage = message.trim();
+
+  if (trimmedMessage === "") {
+    return "Fine. Be that way!";
+  }
+
+  if (
+    trimmedMessage.endsWith("?") &&
+    trimmedMessage.toUpperCase() === trimmedMessage &&
+    trimmedMessage
+      .split("")
+      .some((char) => LOWER_ALPHA.includes(char.toLowerCase()))
+  ) {
+    return "Calm down, I know what I'm doing!";
+  }
+
+  if (trimmedMessage.endsWith("?")) {
+    return "Sure.";
+  }
+
+  if (
+    trimmedMessage.toUpperCase() === trimmedMessage &&
+    trimmedMessage
+      .split("")
+      .some((char) => LOWER_ALPHA.includes(char.toLowerCase()))
+  ) {
+    return "Whoa, chill out!";
+  }
+
+  return "Whatever.";
 }
