17ed2d06618cd93fe615dadaca2407fb2a4496db
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-18 08:41:56 -0500

    ts: resistor color duo

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,20 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
+
+export function decodedValue(colors: string[]): number {
+  const [first, second] = colors;
+  const firstIndex = COLORS.indexOf(first);
+  const secondIndex = COLORS.indexOf(second);
+
+  return Number(`${firstIndex}${secondIndex}`);
 }
