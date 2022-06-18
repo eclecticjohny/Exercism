8c007b195a397f1eefb3efdaa48af0121f64553b
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-18 11:02:04 -0500

    ts: space age

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,3 +1,30 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function age(planet: string, seconds: number): number {
+  const convertedPlanet = convertPlanetToEarthYear(planet);
+
+  return Number((seconds / convertedPlanet).toFixed(2));
+}
+
+function convertPlanetToEarthYear(planet: string): number {
+  const earthYear = 31557600;
+
+  switch (planet) {
+    case "earth":
+      return earthYear;
+    case "mercury":
+      return earthYear * 0.2408467;
+    case "venus":
+      return earthYear * 0.61519726;
+    case "mars":
+      return earthYear * 1.8808158;
+    case "jupiter":
+      return earthYear * 11.862615;
+    case "saturn":
+      return earthYear * 29.447498;
+    case "uranus":
+      return earthYear * 84.016846;
+    case "neptune":
+      return earthYear * 164.79132;
+    default:
+      return 0;
+  }
 }
