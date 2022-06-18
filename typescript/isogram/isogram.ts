e7d88da32da961583e9edbb2d07c3bd0c008f942
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-18 17:15:00 -0500

    ts: isogram

--- typescript/isogram/isogram.ts
+++ typescript/isogram/isogram.ts
@@ -1,5 +1,6 @@
 export function isIsogram(sentence: string): boolean {
   const lettersOnly = sentence.toLowerCase().replace(/[^a-z]/g, "");
   const lettersOnlySet = new Set(lettersOnly);
+
   return lettersOnlySet.size === lettersOnly.length;
 }
