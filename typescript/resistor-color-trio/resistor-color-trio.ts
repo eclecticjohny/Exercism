ce416cdead4e53566d4d27d6ea9e8ea8ddcc95da
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-18 10:14:45 -0500

    ts: resistor color trio

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,24 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
+
+export function decodedResistorValue(resistor: string[]): string {
+  const [firstColor, secondColor, thirdColor] = resistor;
+  const firstIndex = COLORS.indexOf(firstColor);
+  const secondIndex = COLORS.indexOf(secondColor);
+  const thirdIndex = COLORS.indexOf(thirdColor);
+  const zeros = Math.pow(10, thirdIndex);
+  const baseValue = Number(`${firstIndex}${secondIndex}`);
+  const ohmValue = baseValue * zeros;
+
+  return ohmValue >= 1000 ? `${ohmValue / 1000} kiloohms` : `${ohmValue} ohms`;
 }
