7c9266d345627b379cefb1bbdddd416bf68b2d39
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-18 14:35:59 -0500

    ts: triangle

--- typescript/triangle/triangle.ts
+++ typescript/triangle/triangle.ts
@@ -1,17 +1,43 @@
 export class Triangle {
-  constructor(...sides) {
-    throw new Error('Remove this statement and implement this function')
+  sides: number[];
+
+  constructor(...sides: number[]) {
+    this.sides = sides.sort();
   }
 
-  get isEquilateral() {
-    throw new Error('Remove this statement and implement this function')
+  get isEquilateral(): boolean {
+    // return (
+    //   !this.isDegenerate &&
+    //   this.sides.every((side) => side > 0 && side === this.sides[0])
+    // );
+
+    return !this.isDegenerate && new Set(this.sides).size === 1;
   }
 
-  get isIsosceles() {
-    throw new Error('Remove this statement and implement this function')
+  get isIsosceles(): boolean {
+    // return (
+    //   !this.isDegenerate &&
+    //   this.sides.some(
+    //     (side) => this.sides.filter((s) => s > 0 && s === side).length >= 2
+    //   )
+    // );
+
+    return !this.isDegenerate && new Set(this.sides).size <= 2;
   }
 
-  get isScalene() {
-    throw new Error('Remove this statement and implement this function')
+  get isScalene(): boolean {
+    // return (
+    //   !this.isDegenerate &&
+    //   this.sides.every(
+    //     (side) => this.sides.filter((s) => s > 0 && s === side).length === 1
+    //   )
+    // );
+
+    return !this.isDegenerate && new Set(this.sides).size === 3;
+  }
+
+  get isDegenerate(): boolean {
+    const [a, b, c] = this.sides;
+    return this.sides.some((side) => side === 0) || a + b <= c;
   }
 }
