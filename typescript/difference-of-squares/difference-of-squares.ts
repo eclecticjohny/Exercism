15d52ffc6e045cfa89bd1ca23ace060d5cf60a03
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-19 10:00:00 -0500

    ts: difference of squares

--- typescript/difference-of-squares/difference-of-squares.ts
+++ typescript/difference-of-squares/difference-of-squares.ts
@@ -1,17 +1,27 @@
 export class Squares {
-  constructor(count: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  count: number;
+
+  constructor(count: number) {
+    this.count = count;
   }
 
-  get sumOfSquares(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get sumOfSquares(): number {
+    const squaresit = (acc: number, num: number): number =>
+      acc + Math.pow(num, 2);
+    return this.numbersArray.reduce(squaresit, 0);
   }
 
-  get squareOfSum(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get squareOfSum(): number {
+    const sumit = (acc: number, num: number): number => acc + num;
+    const sum = this.numbersArray.reduce(sumit, 0);
+    return Math.pow(sum, 2);
   }
 
-  get difference(): unknown {
-    throw new Error('Remove this statement and implement this function')
+  get difference(): number {
+    return this.squareOfSum - this.sumOfSquares;
+  }
+
+  get numbersArray(): number[] {
+    return Array.from({ length: this.count }, (_, i) => i + 1);
   }
 }
