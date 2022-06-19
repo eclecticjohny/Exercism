389f9e32ef7d4a9c63cb36dda7040e1596aea98d
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-19 10:16:08 -0500

    ts: etl

--- typescript/etl/etl.ts
+++ typescript/etl/etl.ts
@@ -1,3 +1,11 @@
-export function transform(/* Parameters go here */) {
-  throw new Error('Remove this statement and implement this function')
+export function transform(old: { [key: string]: string[] }): {
+  [key: string]: number;
+} {
+  const newObj: { [key: string]: number } = {};
+  for (const key in old) {
+    for (const letter of old[key]) {
+      newObj[letter.toLowerCase()] = Number(key);
+    }
+  }
+  return newObj;
 }
