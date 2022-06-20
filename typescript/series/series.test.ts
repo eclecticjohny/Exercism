14c9213179be06b0355b0164a73101bb7d99bd75
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-19 20:24:01 -0500

    ts: series

--- typescript/series/series.test.ts
+++ typescript/series/series.test.ts
@@ -1,37 +1,37 @@
-import { Series } from './series'
+import { Series } from "./series";
 
-describe('Series', () => {
-  xit('slices of one from one', () => {
-    expect(new Series('1').slices(1)).toEqual([[1]])
-  })
+describe("Series", () => {
+  it("slices of one from one", () => {
+    expect(new Series("1").slices(1)).toEqual([[1]]);
+  });
 
-  xit('slices of one from two', () => {
-    expect(new Series('12').slices(1)).toEqual([[1], [2]])
-  })
+  it("slices of one from two", () => {
+    expect(new Series("12").slices(1)).toEqual([[1], [2]]);
+  });
 
-  xit('slices of two', () => {
-    expect(new Series('35').slices(2)).toEqual([[3, 5]])
-  })
+  it("slices of two", () => {
+    expect(new Series("35").slices(2)).toEqual([[3, 5]]);
+  });
 
-  xit('slices of two overlap', () => {
-    expect(new Series('9142').slices(2)).toEqual([
+  it("slices of two overlap", () => {
+    expect(new Series("9142").slices(2)).toEqual([
       [9, 1],
       [1, 4],
       [4, 2],
-    ])
-  })
+    ]);
+  });
 
-  xit('slices can include duplicates', () => {
-    expect(new Series('777777').slices(3)).toEqual([
+  it("slices can include duplicates", () => {
+    expect(new Series("777777").slices(3)).toEqual([
       [7, 7, 7],
       [7, 7, 7],
       [7, 7, 7],
       [7, 7, 7],
-    ])
-  })
+    ]);
+  });
 
-  xit('slices of long series', () => {
-    expect(new Series('918493904243').slices(5)).toEqual([
+  it("slices of long series", () => {
+    expect(new Series("918493904243").slices(5)).toEqual([
       [9, 1, 8, 4, 9],
       [1, 8, 4, 9, 3],
       [8, 4, 9, 3, 9],
@@ -40,30 +40,30 @@ describe('Series', () => {
       [3, 9, 0, 4, 2],
       [9, 0, 4, 2, 4],
       [0, 4, 2, 4, 3],
-    ])
-  })
+    ]);
+  });
 
-  xit('slice length is too large', () => {
+  it("slice length is too large", () => {
     expect(() => {
-      new Series('12345').slices(6)
-    }).toThrow(new Error('slice length cannot be greater than series length'))
-  })
+      new Series("12345").slices(6);
+    }).toThrow(new Error("slice length cannot be greater than series length"));
+  });
 
-  xit('slice length cannot be zero', () => {
+  it("slice length cannot be zero", () => {
     expect(() => {
-      new Series('12345').slices(0)
-    }).toThrow(new Error('slice length cannot be zero'))
-  })
+      new Series("12345").slices(0);
+    }).toThrow(new Error("slice length cannot be zero"));
+  });
 
-  xit('slice length cannot be negative', () => {
+  it("slice length cannot be negative", () => {
     expect(() => {
-      new Series('123').slices(-1)
-    }).toThrow(new Error('slice length cannot be negative'))
-  })
+      new Series("123").slices(-1);
+    }).toThrow(new Error("slice length cannot be negative"));
+  });
 
-  xit('empty series is invalid', () => {
+  it("empty series is invalid", () => {
     expect(() => {
-      new Series('').slices(1)
-    }).toThrow(new Error('series cannot be empty'))
-  })
-})
+      new Series("").slices(1);
+    }).toThrow(new Error("series cannot be empty"));
+  });
+});
