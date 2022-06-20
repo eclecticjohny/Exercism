572b15343c6a32076c4c4b6a55b2aaeaf1cd8ca6
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-20 11:41:45 -0500

    elixir: username

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,17 +1,23 @@
 defmodule Username do
+  defguard is_lowercase(codepoint) when codepoint >= ?a and codepoint <= ?z
+  defguard is_underscore(codepoint) when codepoint == ?_
+
   @spec sanitize(charlist()) :: charlist()
   def sanitize(username) do
-    Enum.reduce(username, '', fn char, acc ->
-      acc ++
-        case char do
-          ?_ -> '_'
-          ?ä -> 'ae'
-          ?ö -> 'oe'
-          ?ß -> 'ss'
-          ?ü -> 'ue'
-          char when char in ?a..?z -> [char]
-          _ -> ''
-        end
-    end)
+    username
+    |> Enum.map(&substitute_german_chars/1)
+    |> List.flatten()
+    |> Enum.filter(&(is_lowercase(&1) or is_underscore(&1)))
+  end
+
+  @spec substitute_german_chars(charlist()) :: charlist()
+  defp substitute_german_chars(codepoint) do
+    case codepoint do
+      ?ä -> 'ae'
+      ?ö -> 'oe'
+      ?ü -> 'ue'
+      ?ß -> 'ss'
+      _ -> codepoint
+    end
   end
 end
