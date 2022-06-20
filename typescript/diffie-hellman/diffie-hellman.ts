cf4072c141129324588f23349026e5e5fa0daf73
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-19 21:33:22 -0500

    ts: diffie hellman

--- typescript/diffie-hellman/diffie-hellman.ts
+++ typescript/diffie-hellman/diffie-hellman.ts
@@ -1,13 +1,40 @@
 export class DiffieHellman {
-  constructor(p: unknown, g: unknown) {
-    throw new Error('Remove this statement and implement this function')
+  constructor(private p: number, private g: number) {
+    if (p < 2 || !this.isPrime(p)) {
+      throw new Error("p is not prime");
+    }
+    if (g < 2 || g >= p) {
+      throw new Error("g is out of range");
+    }
   }
 
-  public getPublicKey(privateKey: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public getPublicKey(privateKey: number): number {
+    if (privateKey < 0) {
+      throw new Error("private key is negative");
+    }
+    if (privateKey === 0) {
+      throw new Error("private key is zero");
+    }
+    if (privateKey === 1) {
+      throw new Error("private key is one");
+    }
+    if (privateKey === this.p) {
+      throw new Error("private key equals the modulus parameter p");
+    }
+    if (privateKey > this.p) {
+      throw new Error("private key is greater than the modulus parameter p");
+    }
+
+    return Math.pow(this.g, privateKey) % this.p;
   }
 
-  public getSecret(theirPublicKey: unknown, myPrivateKey: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  public getSecret(theirPublicKey: number, myPrivateKey: number): number {
+    return Math.pow(theirPublicKey, myPrivateKey) % this.p;
+  }
+
+  private isPrime(num: number): boolean {
+    for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+      if (num % i === 0) return false;
+    return num > 1;
   }
 }
