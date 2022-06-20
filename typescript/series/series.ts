a1258fdf32228897c7c8133a9c2acb9fb5c799f4
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-19 20:54:53 -0500

    ts: series

--- typescript/series/series.ts
+++ typescript/series/series.ts
@@ -1,9 +1,29 @@
 export class Series {
-  constructor(series: unknown) {
-    throw new Error('Remove this statement and implement this function')
-  }
+  constructor(private series: string) {}
 
-  slices(sliceLength: unknown): unknown {
-    throw new Error('Remove this statement and implement this function')
+  slices(sliceLength: number): number[][] {
+    if (this.series.length === 0) {
+      throw new Error("series cannot be empty");
+    }
+    if (sliceLength > this.series.length) {
+      throw new Error("slice length cannot be greater than series length");
+    }
+    if (sliceLength === 0) {
+      throw new Error("slice length cannot be zero");
+    }
+    if (sliceLength < 0) {
+      throw new Error("slice length cannot be negative");
+    }
+
+    const result: number[][] = [];
+    for (let i = 0; i < this.series.length - sliceLength + 1; i++) {
+      result.push(
+        this.series
+          .slice(i, i + sliceLength)
+          .split("")
+          .map(Number)
+      );
+    }
+    return result;
   }
 }
