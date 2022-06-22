1fce9d2c7239c33d98b9c8c82a506a99a8256493
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-22 09:22:54 -0500

    elixir: basketball website

--- elixir/basketball-website/lib/basketball_website.ex
+++ elixir/basketball-website/lib/basketball_website.ex
@@ -1,9 +1,13 @@
 defmodule BasketballWebsite do
+  @spec extract_from_path(Map.t(), String.t()) :: String.t() | nil
   def extract_from_path(data, path) do
-    # Please implement the extract_from_path/2 function
+    paths = String.split(path, ".")
+    Enum.reduce(paths, data, fn path, acc -> acc[path] end)
   end
 
+  @spec get_in_path(Map.t(), String.t()) :: String.t() | nil
   def get_in_path(data, path) do
-    # Please implement the get_in_path/2 function
+    paths = String.split(path, ".")
+    Kernel.get_in(data, paths)
   end
 end
