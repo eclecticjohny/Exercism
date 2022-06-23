d9e91858d474edb40fa7913003ea58e8c0245911
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-22 19:52:32 -0500

    go: weather forecast

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather is the package weather.
 package weather
 
+// CurrentCondition is the current weather condition.
 var CurrentCondition string
+
+// CurrentLocation is the current location for the forecast.
 var CurrentLocation string
 
+// Forecast is a func to get the forecast for the current location.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
