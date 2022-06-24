1025fbf6fb941b698eba8977cf9901371f776913
Johny Hoffman <johny.hoffman@gmail.com>
2022-06-23 21:59:55 -0500

    go: lasagna master

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,9 +1,33 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, avgPrepTime int) int {
+	if avgPrepTime <= 0 {
+		avgPrepTime = 2
+	}
+	return len(layers) * avgPrepTime
+}
 
-// TODO: define the 'Quantities()' function
+func Quantities(layers []string) (int, float64) {
+	noodles := 0
+	sauce := 0.0
+	for _, layer := range layers {
+		if layer == "noodles" {
+			noodles++
+		} else if layer == "sauce" {
+			sauce++
+		}
+	}
+	return noodles * 50, float64(sauce) * 0.2
+}
 
-// TODO: define the 'AddSecretIngredient()' function
+func AddSecretIngredient(friendList []string, ownList []string) {
+	ownList[len(ownList)-1] = friendList[len(friendList)-1]
+}
 
-// TODO: define the 'ScaleRecipe()' function
+func ScaleRecipe(ingredients []float64, portions int) []float64 {
+	var scaledRecipe []float64
+	for _, ingredient := range ingredients {
+		scaledRecipe = append(scaledRecipe, ingredient/2*float64(portions))
+	}
+	return scaledRecipe
+}
